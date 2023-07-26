4e27562f736bcb6af3507a24a11ab5a07245dc28
eespinoza <eespinoza@grupoinmotion.com>
2023-07-26 10:33:27 -0500

    resuelto pero con fallas

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 // Ejercicio 2
-async function writeFile (filePath, data, callback) {
 
+const fs = require('fs');
+async function writeFile (filePath, data, callback) {
+  
+  const writeStream = fs.createWriteStream(filePath);
+
+  
+  
+  writeStream.write(data, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error al escribir el archivo:', err);
+    } else {
+      console.log('Archivo escrito exitosamente.');
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }
+    writeStream.end();
+  });
 }
 
 // Ejercicio 3
 async function readFileAndCount (word, callback) {
+  if (!word) {
+    return callback(new Error('No se hay palabra a buscar'));
+  }
+
+
+  if (process.argv.length < 3) {
+    return callback(new Error('No se encuentra el path del archivo'));
+  }
+
+  const filePath = process.argv[2];
+
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+
+      if (err.code === 'ENOENT') {
+        return callback(null, 0);
+      } else {
+
+        return callback(err);
+      }
+    }
+
+    const occurrences = (data.match(new RegExp(`\\b${word}\\b`, 'gi')) || []).length;
+    callback(null, occurrences);
+  });
+
+
+
+  readFileAndCount(word, (err, result) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`La palabra "${word}" aparece ${result} veces en el archivo.`);
+    }
+  });
 
 }
 
